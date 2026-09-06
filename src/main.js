8351eb0483dca426dd0c110d6a58cf8dd039f395
Nikhil <nikhiljaiswal200608@gmail.com>
2026-09-06 16:18:43 +0530

    Fix Github pages CSS module loading

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js";
-import "./style.css";
 
 /* =========================================================
    ROAD RUSH: INFINITE
